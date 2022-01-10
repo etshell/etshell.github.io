f98105396aa30a69c86f0ee483141893242ea713
Hasna L <asna.lhermite@gmail.com>
2022-01-10 22:27:19 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -23,9 +23,9 @@ window.onload = function() {
     'Hi there!',
     'Hope you\'re fine',
     'There\'s nothing to see here for the time being.',
-    'I am currently taking a short break to look within, embrace my reality, question and reflect on myself and my circumstances...',
-    'You know, put things in perspective in order to redefine how I would like to grow as a person and as a designer.',
-    'See you soon, I hope!',
+    'I am currently taking a short break to look within, question and reflect on myself and my circumstances... Put things in perspective to redefine how I would like to grow as a person and as a designer.',
+    'If you would like to reach out for whatever reason, feel free to drop me an email at <a href="mailto:hasna.lahmini@gmail.com">contact@hasna.design</a>',
+    'See you soon!',
     getCurrentTime(),
     '🖖'
   ]
